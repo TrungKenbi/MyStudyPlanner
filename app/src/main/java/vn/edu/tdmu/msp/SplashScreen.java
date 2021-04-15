42c83633e1ca8bafbf0c0aa2746be61a4ea91cbf
minhhieu12 <hieuleggo1280@gmail.com>
2021-04-16 01:20:55 +0700

    splash screen

---
+++ app/src/main/java/vn/edu/tdmu/msp/SplashScreen.java
@@ -0,0 +1,46 @@
+package vn.edu.tdmu.msp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_SCREEN = 2000;
+
+    Animation topAnimation, bottomAnimation;
+    ImageView image;
+    TextView text;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_screen);
+
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        image = findViewById(R.id.imageView2);
+        text = findViewById(R.id.textView);
+
+        image.setAnimation(topAnimation);
+        text.setAnimation(bottomAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
+    }
+}
